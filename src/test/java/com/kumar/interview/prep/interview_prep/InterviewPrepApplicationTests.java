c3865b7386ddfdc3ed5beb1034e617d6b01fbd4e
kr68011 <kr68011@gmail.com>
2025-11-01 22:53:45 +0530

    intial commit

---
+++ src/test/java/com/kumar/interview/prep/interview_prep/InterviewPrepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kumar.interview.prep.interview_prep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewPrepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
