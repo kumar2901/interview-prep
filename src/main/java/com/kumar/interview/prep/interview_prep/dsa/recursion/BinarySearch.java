4d3c0ac00ca2533b071a0c628d3ab4f85f9adcd0
kr68011 <kr68011@gmail.com>
2025-11-01 23:03:36 +0530

    recursion basics

---
+++ src/main/java/com/kumar/interview/prep/interview_prep/dsa/recursion/BinarySearch.java
@@ -0,0 +1,35 @@
+package com.kumar.interview.prep.interview_prep.dsa.recursion;
+
+public class BinarySearch {
+
+    /*
+     Search in rotated sorted array
+     may contains duplicates
+     */
+    public int rotatedBinarySearch(int[] arr, int target, int left, int right) {
+        if (left > right) {
+            return -1;
+        }
+        int mid = left + (right - left) / 2;
+        if (arr[mid] == target) {
+            return mid;
+        }
+        if (arr[left] < arr[mid]) {
+            if (target >= arr[left] && target <= arr[mid]) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        } else if (arr[left] > arr[mid]) {
+            if (target >= arr[mid] && target <= arr[right]) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        } else { //difficult part handling duplicates
+            left++;
+
+        }
+        return rotatedBinarySearch(arr, target, left, right);
+    }
+}
