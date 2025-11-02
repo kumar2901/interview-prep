9ad0259d260c9f1598cabe9a90e75300662573d9
kr68011 <kr68011@gmail.com>
2025-11-02 11:25:52 +0530

    code formatter

--- src/main/java/com/kumar/interview/prep/interview_prep/dsa/recursion/Sorting.java
+++ src/main/java/com/kumar/interview/prep/interview_prep/dsa/recursion/Sorting.java
@@ -4,13 +4,13 @@ import java.util.Arrays;
 
 public class Sorting {
 
-    private void bubble(int[] nums, int i, int j) {
+    private void bubbleSort(int[] nums, int i, int j) {
         if (i >= nums.length) {
             return;
         }
         if (j >= nums.length) {
             i = i + 1;
-            bubble(nums, i, i + 1);
+            bubbleSort(nums, i, i + 1);
             return;
         }
         if (nums[i] > nums[j]) {
@@ -19,12 +19,37 @@ public class Sorting {
             nums[j] = temp;
 
         }
-        bubble(nums, i, j + 1);
+        bubbleSort(nums, i, j + 1);
+    }
+
+    private void selectionSort(int[] nums, int i, int j, int minIndex) {
+
+        if (i >= nums.length) {
+            return;
+        }
+        if (j >= nums.length) {
+            int temp = nums[minIndex];
+            nums[minIndex] = nums[i];
+            nums[i] = temp;
+
+            i = i + 1;
+            selectionSort(nums, i, i + 1, i);
+            return;
+        }
+        if (nums[minIndex] > nums[j]) {
+            minIndex = j;
+
+        }
+        selectionSort(nums, i, j + 1, minIndex);
     }
 
     public void main(String[] args) {
         int[] nums = {1, 3, 54, 65, 43, 5, 7, 8};
-        bubble(nums, 0, 1);
+        bubbleSort(nums, 0, 1);
         System.out.println(Arrays.toString(nums));
+
+        int[] nums1 = {1, 3, 54, 65, 43, 5, 7, 8};
+        selectionSort(nums1, 0, 1, 0);
+        System.out.println(Arrays.toString(nums1));
     }
 }
