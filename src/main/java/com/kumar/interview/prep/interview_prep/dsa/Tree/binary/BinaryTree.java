61c1b4400daa2106c53e9a6b241344459d99ea88
kr68011 <kr68011@gmail.com>
2025-12-01 23:56:23 +0530

    Binary Tree

---
+++ src/main/java/com/kumar/interview/prep/interview_prep/dsa/Tree/binary/BinaryTree.java
@@ -0,0 +1,50 @@
+package com.kumar.interview.prep.interview_prep.dsa.Tree.binary;
+
+import com.kumar.interview.prep.interview_prep.dsa.model.TreeNode;
+
+import java.util.*;
+
+public class BinaryTree {
+
+    /**
+     * You are given a root of a binary tree that has n number of nodes. You have to return the right-side view in the
+     * form of a list. A right-side view of a binary tree is the data of the nodes that are visible when the tree is
+     * viewed from the right side.
+     *
+     * @param root
+     *            root
+     * @return right side view
+     */
+    private List<Integer> rightSideView(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+
+        Deque<TreeNode> queue = new LinkedList<>();
+
+        queue.add(root);
+
+        while (!queue.isEmpty()) {
+
+            int size = queue.size();
+            int count = 0;
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+
+                count++;
+
+                if (count == size) {
+                    result.add(node.getVal());
+                }
+                if (node.getLeft() != null) {
+                    queue.offer(node.getLeft());
+                }
+                if (node.getRight() != null) {
+                    queue.offer(node.getRight());
+                }
+
+            }
+
+        }
+
+        return result;
+    }
+}
--- src/main/java/com/kumar/interview/prep/interview_prep/dsa/model/TreeNode.java
+++ src/main/java/com/kumar/interview/prep/interview_prep/dsa/model/TreeNode.java
@@ -7,7 +7,8 @@ public class TreeNode {
     int val;
     TreeNode left;
     TreeNode right;
-    TreeNode(int x) {
+
+    public TreeNode(int x) {
         val = x;
     }
 
