3a20d481d4ee1f51ff2fe758a824503dc1af4c55
kr68011 <kr68011@gmail.com>
2026-08-22 12:19:40 +0530

    lru cache

---
+++ src/main/java/com/kumar/interview/prep/dsa/cache/LRUCacheDemo.java
@@ -0,0 +1,113 @@
+package com.kumar.interview.prep.dsa.cache;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+
+/**
+ *
+ * Implement LRU Cache → make it thread-safe
+ */
+
+class LRUCache<K, V> {
+    private class Node<K, V> {
+        K key;
+        V value;
+        Node<K, V> next;
+        Node<K, V> prev;
+
+        Node(K k, V v) {
+            this.key = k;
+            this.value = v;
+        }
+
+        @Override
+        public String toString() {
+            return key + "=" + value;
+        }
+    }
+
+    int capacity;
+    Map<K, Node<K, V>> map;
+    private static final int DEFAULT_CAPACITY = 5;
+    Node<K, V> head;
+    Node<K, V> tail;
+
+    public LRUCache() {
+        this(DEFAULT_CAPACITY);
+    }
+
+    public LRUCache(int capacity) {
+        this.capacity = capacity;
+        map = new HashMap<>();
+        head = new Node(0, "-1");
+        tail = new Node(0, "-1");
+
+        head.next = tail;
+        tail.prev = head;
+    }
+
+    public V get(K key) {
+        if (!map.containsKey(key)) {
+            return null;
+        }
+        Node<K, V> node = map.get(key);
+        moveToHead(node);
+
+        return node.value;
+    }
+
+    private void moveToHead(Node<K, V> node) {
+        node.next = head.next;
+        node.prev = head;
+
+        head.next.prev = node;
+        head.next = node;
+    }
+
+    public V getOeDefault(K key, V defaultValue) {
+        V value = this.get(key);
+        return (value != null) ? value : defaultValue;
+    }
+
+    public void put(K key, V value) {
+        if (map.containsKey(key)) {
+            Node<K, V> node = map.get(key);
+            node.value = value; // update value
+            moveToHead(node);
+            return;
+        }
+
+        if (map.size() >= capacity) {
+            Node<K, V> tailNode = tail.prev;
+            removeNode(tailNode);
+            map.remove(tailNode.key);
+
+        }
+        Node<K, V> node = new Node<>(key, value);
+        map.put(key, node);
+        moveToHead(node);
+    }
+
+    private void removeNode(Node<K, V> node) {
+        node.prev.next = node.next;
+        node.next.prev = node.prev;
+    }
+
+}
+public class LRUCacheDemo {
+
+    static void main() {
+        LRUCache<Integer,String> lruCache=new LRUCache<>(3);
+        lruCache.put(1, "1");
+        lruCache.put(2, "2");
+        lruCache.put(3, "3");
+        System.out.println(lruCache.get(1));
+
+        System.out.println(lruCache.get(2));
+
+        lruCache.put(4, "4");
+        System.out.println(lruCache.get(4));
+
+    }
+}
